b637532522ca9ad7e045faa63aec9a95cc39eb50
kan15051 <kantaphon505@gmail.com>
2025-08-07 10:30:50 +0700

    added test for useCounter

---
+++ vitest.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    include: ['tests/**/*.test.ts'],
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+    },
+    globals: true,
+    setupFiles: './tests/setup.ts',
+  },
+});
